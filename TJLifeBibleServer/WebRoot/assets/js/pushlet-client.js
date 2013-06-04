97fc15fb7419e4a2a04cdf20119784481867c47f
JingkaiZhao <jingkai.zhao@foxmail.com>
2013-06-04 16:22:51 +0800

    use pushlet push in map.js, unfinished

--- TJLifeBibleServer/WebRoot/assets/js/pushlet-client.js
+++ TJLifeBibleServer/WebRoot/assets/js/pushlet-client.js
@@ -1,11 +1,19 @@
-window.onload = function() {
-	PL._init();
-	PL.joinListen('/message'); 
-    function onData(event) { 
-    	if(event!=null) {
-    		console.log('not null');
-    	} else {
-    		console.log('null');
-    	}
-    } 
-};
+PL._init();
+PL.joinListen('/message');
+function onData(event) {
+	if (event != null) {
+		var messageJson = {
+				'comments': event.get('comments'), 
+				'content': event.get('content'), 
+				'createTime': event.get('createTime'),
+				'creater': event.get('creater'), 
+				'id': event.get('id'),
+				'lat': event.get('lat'), 
+				'lng': event.get('lng')
+		};
+		console.log(messageJson);
+//		top.frames["mapFrame"].window.messagePush(messageJson);
+	} else {
+		console.log('error');
+	}
+}
